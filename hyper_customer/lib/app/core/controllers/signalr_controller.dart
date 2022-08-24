f19d3369b50040e1ac12a94686eb277f86799770
= <=>
2022-08-24 22:09:41 +0700

    gg

--- hyper_customer/lib/app/core/controllers/signalr_controller.dart
+++ hyper_customer/lib/app/core/controllers/signalr_controller.dart
@@ -32,8 +32,10 @@ class SignalR {
   /// Describes the current state of the HubConnection to the server.
   Rx<HubState> hubState = HubState.disconnected.obs;
 
-  final String _hubUrl =
-      "https://tourism-smart-transportation-api.azurewebsites.net/hub";
+  // final String _hubUrl =
+  //     "https://tourism-smart-transportation-api.azurewebsites.net/hub";
+
+  final String _hubUrl = "https://localhost:5001/hub";
 
   // Handle on resume
   _handleAppLifecycleState() {
