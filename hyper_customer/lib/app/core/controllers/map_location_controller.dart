55ea75752132dcdfb34e89ebb6857ba662fbd4d6
= <=>
2022-08-27 12:31:55 +0700

    gg

--- hyper_customer/lib/app/core/controllers/map_location_controller.dart
+++ hyper_customer/lib/app/core/controllers/map_location_controller.dart
@@ -13,6 +13,12 @@ class MapLocationController {
   StreamSubscription<Position>? _positionStream;
 
   Future<void> init() async {
+    _positionStream?.cancel();
+    _positionStream = Geolocator.getPositionStream().listen(
+      (Position? position) async {
+        location = LatLng(position?.latitude ?? 0, position?.longitude ?? 0);
+      },
+    );
     await loadLocation();
   }
 
@@ -25,31 +31,24 @@ class MapLocationController {
   Future<bool> loadLocation() async {
     if (_positionStream != null) return true;
 
-    permission = await Geolocator.checkPermission();
+    try {
+      permission = await Geolocator.checkPermission();
 
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        await _deniedDialog();
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          await _deniedDialog();
+        }
       }
-    }
 
-    if (permission == LocationPermission.deniedForever) {
-      await _deniedForeverDialog();
+      if (permission == LocationPermission.deniedForever) {
+        await _deniedForeverDialog();
+      }
+    } catch (e) {
+      // TO DO
     }
-
-    _positionStream?.cancel();
-    _positionStream = Geolocator.getPositionStream().listen(
-      (Position? position) async {
-        location = LatLng(position?.latitude ?? 0, position?.longitude ?? 0);
-      },
-    );
 
     return true;
-  }
-
-  Future<LatLng> getCurrentLocation() async {
-    return location ?? LatLng(0, 0);
   }
 
   Future<void> _deniedDialog() async {
